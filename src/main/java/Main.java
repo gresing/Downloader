c5730753cb6487f1eacb060c655fd7c34acc21bd
SergeyShkuratko <shkuratko@haulmont.com>
2016-11-16 15:33:36 +0400

    develop step

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,21 @@
+import downloadmanager.DownloadManager;
+import downloadmanager.DownloadManagerImpl;
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String argsString = Arrays.toString(args);
-        String[] argPairs = argsString.split("-");
-
-        for (String arg : argPairs) {
-            String[] pair = arg.trim().split(" ");
-
-            System.out.println(pair[0] + " par name");
-            System.out.println(pair[1] + " par value");
+        String argsString = "";
+        int count = 0;
+        for (String string : args) {
+            argsString = argsString + string;
+            count++;
+            if (count != args.length) {
+                argsString = argsString + " ";
+            }
         }
+        String[] tempArray = argsString.split("-");
+        String[] argPairs = Arrays.copyOfRange(tempArray, 1, tempArray.length);
+        DownloadManager dm = new DownloadManagerImpl(argPairs);
     }
 }
--- src/main/java/downloadmanager/DownloadManagerImpl.java
+++ src/main/java/downloadmanager/DownloadManagerImpl.java
@@ -17,7 +17,49 @@ public class DownloadManagerImpl implements DownloadManager {
         this.destination = destination;
     }
 
+    public DownloadManagerImpl(String[] argPairs) {
+        if (argPairs.length != 4) {
+            throw new IllegalArgumentException("Arguments must contain 4 values. For example \"-n 5 -l 2000k -o output_folder -f links.txt\"");
+        }
+        parseArgPairs(argPairs);
+        System.out.println(this);
+    }
+
+    private void parseArgPairs(String[] argPairs) {
+        for (String args : argPairs) {
+            String[] arg = args.split(" ");
+            switch (arg[0]) {
+                case "n":
+                    this.threadCount = parseInt(arg[1]);
+                    break;
+                case "l":
+                    this.speedLimit = parseInt(arg[1]);
+                    break;
+                case "o":
+                    this.destination = arg[1];
+                    break;
+                case "f":
+                    this.loadFilePath = arg[1];
+                    break;
+                default:
+                    throw new IllegalArgumentException("Incorrect argument " + args);
+
+            }
+        }
+    }
+
     public LoadStatus loadFile(DownloadFile downloadFile) {
         return null;
+    }
+
+    private int parseInt(String stringValue) {
+        stringValue.endsWith("k");
+        int value;
+        try {
+            value = Integer.parseInt(stringValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Expected integer argument. Received " + stringValue, e);
+        }
+        return value;
     }
 }
